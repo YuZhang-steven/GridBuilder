656e7df977f8ba76138cd74273929af278604d76
S.Zhang <zengeryounke@hotmail.com>
2023-04-30 22:51:35 -0700

    cube surface function done

--- src/Cube.jsx
+++ src/Cube.jsx
@@ -2,7 +2,12 @@ import CubeFace from "./CubeFace";
 
 export default function Cube(props) {
     return <>
-        <CubeFace start_pos={[1, 1, 0]} dir={'front'} />
+        <CubeFace start_pos={[0, 0, 0]} dir={'front'} />
+        <CubeFace start_pos={[0, 0, 0]} dir={'back'} />
+        <CubeFace start_pos={[0, 0, 0]} dir={'top'} />
+        <CubeFace start_pos={[0, 0, 0]} dir={'bottom'} />
+        <CubeFace start_pos={[0, 0, 0]} dir={'left'} />
+        <CubeFace start_pos={[0, 0, 0]} dir={'right'} />
         {/* <boxGeometry /> */}
     </>
 }
--- src/CubeFace.jsx
+++ src/CubeFace.jsx
@@ -1,167 +1,118 @@
-let start_pos = [0, 0, 0]
-
-const front = new Float32Array([
-    start_pos[0] + 1, start_pos[1] + 1, start_pos[2],
-    start_pos[0] + 1, start_pos[1], start_pos[2],
-    start_pos[0], start_pos[1], start_pos[2],
-
-
-    start_pos[0], start_pos[1], start_pos[2],
-    start_pos[0], start_pos[1] + 1, start_pos[2],
-    start_pos[0] + 1, start_pos[1] + 1, start_pos[2],
-])
-const back = new Float32Array([
-    start_pos[0], start_pos[1], start_pos[2] + 1,
-    start_pos[0] + 1, start_pos[1], start_pos[2] + 1,
-    start_pos[0] + 1, start_pos[1] + 1, start_pos[2] + 1,
+/** create a flot number vertice array
+ * input: start position of the cube(left fron corner)
+ *        dir: direction of the face
+ * output: a Float32Array
+ */
+const createArray = (start_pos, dir) => {
+    switch (dir) {
+        case 'front':
+            return new Float32Array([
+                start_pos[0] + 1, start_pos[1] + 1, start_pos[2],
+                start_pos[0] + 1, start_pos[1], start_pos[2],
+                start_pos[0], start_pos[1], start_pos[2],
 
 
-    start_pos[0] + 1, start_pos[1] + 1, start_pos[2] + 1,
-    start_pos[0], start_pos[1] + 1, start_pos[2] + 1,
-    start_pos[0], start_pos[1], start_pos[2] + 1,
-])
+                start_pos[0], start_pos[1], start_pos[2],
+                start_pos[0], start_pos[1] + 1, start_pos[2],
+                start_pos[0] + 1, start_pos[1] + 1, start_pos[2],
+            ])
 
-const top = new Float32Array([
-    start_pos[0] + 1, start_pos[1] + 1, start_pos[2],
-    start_pos[0], start_pos[1] + 1, start_pos[2],
-    start_pos[0], start_pos[1] + 1, start_pos[2] + 1,
-
-
-    start_pos[0] + 1, start_pos[1] + 1, start_pos[2],
-    start_pos[0], start_pos[1] + 1, start_pos[2] + 1,
-    start_pos[0] + 1, start_pos[1] + 1, start_pos[2] + 1,
-
-])
-
-const bottom = new Float32Array([
-    start_pos[0], start_pos[1], start_pos[2],
-    start_pos[0] + 1, start_pos[1], start_pos[2],
-    start_pos[0], start_pos[1], start_pos[2] + 1,
-
-    start_pos[0], start_pos[1], start_pos[2] + 1,
-    start_pos[0] + 1, start_pos[1], start_pos[2],
-    start_pos[0] + 1, start_pos[1], start_pos[2] + 1,
-
-])
-
-const left = new Float32Array([
-    start_pos[0], start_pos[1], start_pos[2],
-    start_pos[0], start_pos[1], start_pos[2] + 1,
-    start_pos[0], start_pos[1] + 1, start_pos[2] + 1,
-
-    start_pos[0], start_pos[1] + 1, start_pos[2] + 1,
-    start_pos[0], start_pos[1] + 1, start_pos[2],
-    start_pos[0], start_pos[1], start_pos[2],
-
-])
-const right = new Float32Array([
-    start_pos[0] + 1, start_pos[1], start_pos[2] + 1,
-    start_pos[0] + 1, start_pos[1], start_pos[2],
-    start_pos[0] + 1, start_pos[1] + 1, start_pos[2] + 1,
-
-    start_pos[0] + 1, start_pos[1] + 1, start_pos[2] + 1,
-    start_pos[0] + 1, start_pos[1], start_pos[2],
-    start_pos[0] + 1, start_pos[1] + 1, start_pos[2],
-
-])
-
-const positions = new Float32Array([
-    0, 0, 0,
-    1, 0, 0,
-    0, 1, 0,
-
-    0, 1, 0,
-    1, 0, 0,
-    1, 1, 0,
-
-])
+        case 'back':
+            return new Float32Array([
+                start_pos[0], start_pos[1], start_pos[2] + 1,
+                start_pos[0] + 1, start_pos[1], start_pos[2] + 1,
+                start_pos[0] + 1, start_pos[1] + 1, start_pos[2] + 1,
 
 
+                start_pos[0] + 1, start_pos[1] + 1, start_pos[2] + 1,
+                start_pos[0], start_pos[1] + 1, start_pos[2] + 1,
+                start_pos[0], start_pos[1], start_pos[2] + 1,
+            ])
 
+
+        case 'top':
+            return new Float32Array([
+                start_pos[0] + 1, start_pos[1] + 1, start_pos[2],
+                start_pos[0], start_pos[1] + 1, start_pos[2],
+                start_pos[0], start_pos[1] + 1, start_pos[2] + 1,
+
+
+                start_pos[0] + 1, start_pos[1] + 1, start_pos[2],
+                start_pos[0], start_pos[1] + 1, start_pos[2] + 1,
+                start_pos[0] + 1, start_pos[1] + 1, start_pos[2] + 1,
+
+            ])
+
+        case 'bottom':
+            return new Float32Array([
+                start_pos[0], start_pos[1], start_pos[2],
+                start_pos[0] + 1, start_pos[1], start_pos[2],
+                start_pos[0], start_pos[1], start_pos[2] + 1,
+
+                start_pos[0], start_pos[1], start_pos[2] + 1,
+                start_pos[0] + 1, start_pos[1], start_pos[2],
+                start_pos[0] + 1, start_pos[1], start_pos[2] + 1,
+
+            ])
+
+
+        case 'left':
+            return new Float32Array([
+                start_pos[0], start_pos[1], start_pos[2],
+                start_pos[0], start_pos[1], start_pos[2] + 1,
+                start_pos[0], start_pos[1] + 1, start_pos[2] + 1,
+
+                start_pos[0], start_pos[1] + 1, start_pos[2] + 1,
+                start_pos[0], start_pos[1] + 1, start_pos[2],
+                start_pos[0], start_pos[1], start_pos[2],
+
+            ])
+
+        case 'right':
+            return new Float32Array([
+                start_pos[0] + 1, start_pos[1], start_pos[2] + 1,
+                start_pos[0] + 1, start_pos[1], start_pos[2],
+                start_pos[0] + 1, start_pos[1] + 1, start_pos[2] + 1,
+
+                start_pos[0] + 1, start_pos[1] + 1, start_pos[2] + 1,
+                start_pos[0] + 1, start_pos[1], start_pos[2],
+                start_pos[0] + 1, start_pos[1] + 1, start_pos[2],
+
+            ])
+    }
+
+}
+
+
+
+/*
+Create one cube surface from the location and the direction.
+The surface create from the vertices, which directly transfer to two triangle surfaces
+
+
+props: 
+    start_pos: frot bottom left corner cordination array
+    dir: direction of the surface
+
+output: a buffergeometry surface
+*/
 export default function CubeFace(props) {
-    start_pos = props.start_pos
+    const start_pos = props.start_pos
     const dir = props.dir
-    // console.log(start_pos);
-    return <>
-        <mesh position={props.position}>
-            <bufferGeometry>
-                <bufferAttribute
-                    attach={'attributes-position'}
-                    array={front}
-                    count={front.length / 3}
-                    itemSize={3}
 
-                />
-            </bufferGeometry>
-            {/* <planeGeometry /> */}
-            <meshBasicMaterial />
-        </mesh>
-        <mesh position={props.position}>
-            <bufferGeometry>
-                <bufferAttribute
-                    attach={'attributes-position'}
-                    array={back}
-                    count={back.length / 3}
-                    itemSize={3}
+    let vertices = createArray(start_pos, dir)//getr
 
-                />
-            </bufferGeometry>
-            {/* <planeGeometry /> */}
-            <meshBasicMaterial color={"yellow"} />
-        </mesh>
-        <mesh position={props.position}>
-            <bufferGeometry>
-                <bufferAttribute
-                    attach={'attributes-position'}
-                    array={top}
-                    count={top.length / 3}
-                    itemSize={3}
+    return <mesh position={props.position}>
+        <bufferGeometry>
+            <bufferAttribute
+                attach={'attributes-position'}
+                array={vertices}
+                count={vertices.length / 3}
+                itemSize={3}
 
-                />
-            </bufferGeometry>
-            {/* <planeGeometry /> */}
-            <meshBasicMaterial color={"red"} />
-        </mesh>
-        <mesh position={props.position}>
-            <bufferGeometry>
-                <bufferAttribute
-                    attach={'attributes-position'}
-                    array={bottom}
-                    count={bottom.length / 3}
-                    itemSize={3}
+            />
+        </bufferGeometry>
+        <meshBasicMaterial wireframe />
+    </mesh>
 
-                />
-            </bufferGeometry>
-            {/* <planeGeometry /> */}
-            <meshBasicMaterial color={"blue"} />
-        </mesh>
-        <mesh position={props.position}>
-            <bufferGeometry>
-                <bufferAttribute
-                    attach={'attributes-position'}
-                    array={left}
-                    count={left.length / 3}
-                    itemSize={3}
-
-                />
-            </bufferGeometry>
-            {/* <planeGeometry /> */}
-            <meshBasicMaterial color={"purple"} />
-        </mesh>
-        <mesh position={props.position}>
-            <bufferGeometry>
-                <bufferAttribute
-                    attach={'attributes-position'}
-                    array={right}
-                    count={right.length / 3}
-                    itemSize={3}
-
-                />
-            </bufferGeometry>
-            {/* <planeGeometry /> */}
-            <meshBasicMaterial color={"cyan"} />
-        </mesh>
-
-
-    </>
 }
