78db7bc2634c8d02f1b96fcd9ef7eeb2201ff8dc
S.Zhang <zengeryounke@hotmail.com>
2023-05-02 00:38:37 -0700

    cube system generate from an array

--- src/Cube.jsx
+++ src/Cube.jsx
@@ -1,13 +1,21 @@
 import CubeFace from "./CubeFace";
 
+/**
+ * 
+ * @param {*} props 
+ * pos: position(left, front and bottom most corner cordination(3 entries array))
+ * 
+ * @returns Box gyometry with 6 faces.
+ */
 export default function Cube(props) {
+    const position = props.pos
     return <>
-        <CubeFace start_pos={[0, 0, 0]} dir={'front'} />
-        <CubeFace start_pos={[0, 0, 0]} dir={'back'} />
-        <CubeFace start_pos={[0, 0, 0]} dir={'top'} />
-        <CubeFace start_pos={[0, 0, 0]} dir={'bottom'} />
-        <CubeFace start_pos={[0, 0, 0]} dir={'left'} />
-        <CubeFace start_pos={[0, 0, 0]} dir={'right'} />
-        {/* <boxGeometry /> */}
+        {/* generate box face with start position and a normal direction. */}
+        <CubeFace start_pos={position} dir={'front'} />
+        <CubeFace start_pos={position} dir={'back'} />
+        <CubeFace start_pos={position} dir={'top'} />
+        <CubeFace start_pos={position} dir={'bottom'} />
+        <CubeFace start_pos={position} dir={'left'} />
+        <CubeFace start_pos={position} dir={'right'} />
     </>
 }
--- src/GridSystem.jsx
+++ src/GridSystem.jsx
@@ -1,11 +1,38 @@
 import Cube from "./Cube"
 
+/**
+ * Generate a cube system with x,y,z range.
+ * The whole system will be a bunch box
+ * @param {*} props 
+ * x: the number of box on x 
+ * y: the number of box on y
+ * x: the number of box on z
+ * @returns 
+ * a bunch box with six faces.
+ */
 export default function GridSystem(props) {
+    /** The size of the grid */
     const range_x = props.x
     const range_y = props.y
     const range_z = props.z
-    return <>
 
-        <Cube />
+    const position_arr = []// the array include all box corner vertices
+
+    /**For loop to fill the start point array */
+    for (let x = 0; x < range_x; x++) {
+        for (let y = 0; y < range_y; y++) {
+            for (let z = 0; z < range_z; z++) {
+                position_arr.push([x, y, z])
+            }
+        }
+    }
+
+    /** transform the start point array to a Cube components array */
+    const cube_list = position_arr.map(
+        postion => <Cube pos={postion} key={postion.toString()} />
+    )
+
+    return <>
+        {cube_list}
     </>
 }
